85661e86a4acb5a5df576b0429525acca76d9bf1
Julian <julianrodriguezvillamiz@gmail.com>
2020-11-22 14:39:20 -0500

    Respaldo ampliado

--- respaldo.py
+++ respaldo.py
@@ -1,71 +1,164 @@
-#    while oper2 != '=' and oper2 == '-' or '+' or '*' or '/' or '+-' or '-+':
-#        date3 = int(input("ingrese número: "))
-#        oper3 = str(input("Ingrese '=' o '%': "))
+date1=1
+date2=1
+date3=1
+oper=input()
+oper2=input()
+oper3=input()
+
+while oper2 != '=' and oper3 != '=':
+    date4 = int(input("ingrese número: "))
+    oper4 = str(input("Ingrese '=': "))
+    
+    print(" ")
+    print("Su resultado es: ")
+    print(" ")
 
     #Suma
-#        if oper == '+' and oper2 == '+' and oper3 == '=':
-#            print(date1 + date2 + date3)
-#        elif oper == '+' and oper2 == '-' and oper3 == '=':
-#            print(date1 + date2 - date3)
-#        elif oper == '+' and oper2 == '*' and oper3 == '=':
-#            print(date1 + date2 * date3)
-#        elif oper == '+' and oper2 == '/' and oper3 == '=':
-#            print(date1 + date2 / date3)
-#        elif oper == '+' and oper2 == '+-' or '-+' and oper3 == '=':
-#            print(date1 + date2 + date3)
-#            print(date1 + date2 - date3)
-    #Resta
-#        elif oper == '-' and oper2 == '+' and oper3 == '=':
-#            print(date1 - date2 + date3)
-#        elif oper == '-' and oper2 == '-' and oper3 == '=':
-#            print(date1 - date2 - date3)
-#        elif oper == '-' and oper2 == '*' and oper3 == '=':
-#            print(date1 - date2 * date3)
-#        elif oper == '-' and oper2 == '/' and oper3 == '=':
-#            print(date1 - date2 / date3)
-#        elif oper == '-' and oper2 == '-' and oper3 == '=':
-#            print(date1 - date2 + date3)
-#            print(date1 - date2 - date3)
-    #Multiplicacion
-#        elif oper == '*' and oper2 == '+' and oper3 == '=':
-#           print(date1 * date2 + date3)
-#        elif oper == '*' and oper2 == '-' and oper3 == '=':
-#           print(date1 * date2 - date3)
-#        elif oper == '*' and oper2 == '*' and oper3 == '=':
-#           print(date1 * date2 * date3)
-#        elif oper == '*' and oper2 == '/' and oper3 == '=':
-#           print(date1 * date2 / date3)
-#        elif oper == '*' and oper2 == '+-' or '-+' and oper3 == '=':
-#           print(date1 * date2 + date3)
-#           print(date1 * date2 - date3)
-    #Division
-#        elif oper == '/' and oper2 == '+' and oper3 == '=':
-#            print(date1 / date2 + date3)
-#        elif oper == '/' and oper2 == '-' and oper3 == '=':
-#            print(date1 / date2 - date3)
-#        elif oper == '/' and oper2 == '*' and oper3 == '=':
-#            print(date1 / date2 * date3)
-#        elif oper == '/' and oper2 == '/' and oper3 == '=':
-#            print(date1 / date2 / date3)
-#        elif oper == '/' and oper2 == '+-' or '-+' and oper3 == '=':
-#            print(date1 / date2 + date3)
-#            print(date1 / date2 - date3)
+    #Suma y suma
+    if oper == '+' and oper2 == '+' and oper3 == '+' and oper4 == '=':
+        print(date1 + date2 + date3 + date4)
+    elif oper == '+' and oper2 == '+' and oper3 == '-' and oper4 == '=':
+        print(date1 + date2 + date3 - date4)
+    elif oper == '+' and oper2 == '+' and oper3 == '*' and oper4 == '=':
+        print(date1 + date2 + date3 * date4)
+    elif oper == '+' and oper2 == '+' and oper3 == '/' and oper4 == '=':
+        print(date1 + date2 + date3 / date4)
     #Suma y resta
-#        elif oper == '+-' or '-+' and oper2 == '+' and oper3 == '=':
-#            print(date1 + date2 + date3)
-#            print(date1 - date2 + date3)
-#        elif oper == '+-' or '-+' and oper2 == '-' and oper3 == '=':
-#            print(date1 + date2 - date3)
-#            print(date1 - date2 - date3)
-#        elif oper == '+-' or '-+' and oper2 == '*' and oper3 == '=':
-#            print(date1 + date2 * date3)
-#            print(date1 - date2 * date3)
-#        elif oper == '+-' or '-+' and oper2 == '/' and oper3 == '=':
-#            print(date1 + date2 / date3)
-#            print(date1 - date2 / date3)
-#        elif oper == '+-' or '-+' and oper2 == '*' and oper3 == '=':
-#            print(date1 + date2 + date3)
-#            print(date1 - date2 + date3)
-#            print(date1 + date2 - date3)
-#            print(date1 - date2 - date3)
-#        break
+    elif oper == '+' and oper2 == '-' and oper3 == '+' and oper4 == '=':
+        print(date1 + date2 - date3 + date4)
+    elif oper == '+' and oper2 == '-' and oper3 == '-' and oper4 == '=':
+        print(date1 + date2 - date3 - date4)
+    elif oper == '+' and oper2 == '-' and oper3 == '*' and oper4 == '=':
+        print(date1 + date2 - date3 * date4)
+    elif oper == '+' and oper2 == '-' and oper3 == '/' and oper4 == '=':
+        print(date1 + date2 - date3 / date4)
+    #Suma y multiplicacion
+    elif oper == '+' and oper2 == '*' and oper3 == '+' and oper4 == '=':
+        print(date1 + date2 * date3 + date4)
+    elif oper == '+' and oper2 == '*' and oper3 == '-' and oper4 == '=':
+        print(date1 + date2 * date3 - date4)
+    elif oper == '+' and oper2 == '*' and oper3 == '*' and oper4 == '=':
+        print(date1 + date2 * date3 * date4)
+    elif oper == '+' and oper2 == '*' and oper3 == '/' and oper4 == '=':
+        print(date1 + date2 * date3 / date4)
+    #Suma y division
+    elif oper == '+' and oper2 == '/' and oper3 == '+' and oper4 == '=':
+        print(date1 + date2 / date3 + date4)
+    elif oper == '+' and oper2 == '/' and oper3 == '-' and oper4 == '=':
+        print(date1 + date2 / date3 + date4)
+    elif oper == '+' and oper2 == '/' and oper3 == '*' and oper4 == '=':
+        print(date1 + date2 / date3 * date4)
+    elif oper == '+' and oper2 == '/' and oper3 == '/' and oper4 == '=':
+        print(date1 + date2 / date3 / date4)
+    #Resta
+    #Resta y suma
+    elif oper == '-' and oper2 == '+' and oper3 == '+' and oper4 == '=':
+        print(date1 - date2 + date3 + date4)
+    elif oper == '-' and oper2 == '+' and oper3 == '-' and oper4 == '=':
+        print(date1 - date2 + date3 - date4)
+    elif oper == '-' and oper2 == '+' and oper3 == '*' and oper4 == '=':
+        print(date1 - date2 + date3 * date4)
+    elif oper == '-' and oper2 == '+' and oper3 == '/' and oper4 == '=':
+        print(date1 - date2 + date3 / date4)
+    #Resta y resta
+    elif oper == '-' and oper2 == '-' and oper3 == '+' and oper4 == '=':
+        print(date1 - date2 - date3 + date4)
+    elif oper == '-' and oper2 == '-' and oper3 == '-' and oper4 == '=':
+        print(date1 - date2 - date3 - date4)
+    elif oper == '-' and oper2 == '-' and oper3 == '*' and oper4 == '=':
+        print(date1 - date2 - date3 * date4)
+    elif oper == '-' and oper2 == '-' and oper3 == '/' and oper4 == '=':
+        print(date1 - date2 - date3 / date4)
+    #Resta y multiplicacion
+    elif oper == '-' and oper2 == '*' and oper3 == '+' and oper4 == '=':
+        print(date1 - date2 * date3 + date4)
+    elif oper == '-' and oper2 == '*' and oper3 == '-' and oper4 == '=':
+        print(date1 - date2 * date3 - date4)
+    elif oper == '-' and oper2 == '*' and oper3 == '*' and oper4 == '=':
+        print(date1 - date2 * date3 * date4)
+    elif oper == '-' and oper2 == '*' and oper3 == '/' and oper4 == '=':
+        print(date1 - date2 * date3 / date4)
+    #Resta y division
+    elif oper == '-' and oper2 == '/' and oper3 == '+' and oper4 == '=':
+        print(date1 - date2 / date3 + date4)
+    elif oper == '-' and oper2 == '/' and oper3 == '-' and oper4 == '=':
+        print(date1 - date2 / date3 - date4)
+    elif oper == '-' and oper2 == '/' and oper3 == '*' and oper4 == '=':
+        print(date1 - date2 / date3 * date4)
+    elif oper == '-' and oper2 == '/' and oper3 == '/' and oper4 == '=':
+        print(date1 - date2 / date3 / date4)
+    #Multiplicacion
+    #Multiplicacion y suma
+    elif oper == '*' and oper2 == '+' and oper3 == '+' and oper4 == '=':
+        print(date1 * date2 + date3 + date4)
+    elif oper == '*' and oper2 == '+' and oper3 == '-' and oper4 == '=':
+        print(date1 * date2 + date3 - date4)
+    elif oper == '*' and oper2 == '+' and oper3 == '*' and oper4 == '=':
+        print(date1 * date2 + date3 * date4)
+    elif oper == '*' and oper2 == '+' and oper3 == '/' and oper4 == '=':
+        print(date1 * date2 + date3 / date4)
+    #Multiplicacion y resta
+    elif oper == '*' and oper2 == '-' and oper3 == '+' and oper4 == '=':
+        print(date1 * date2 - date3 + date4)
+    elif oper == '*' and oper2 == '-' and oper3 == '-' and oper4 == '=':
+        print(date1 * date2 - date3 - date4)
+    elif oper == '*' and oper2 == '-' and oper3 == '*' and oper4 == '=':
+        print(date1 * date2 - date3 * date4)
+    elif oper == '*' and oper2 == '-' and oper3 == '/' and oper4 == '=':
+        print(date1 * date2 - date3 / date4)
+    #Multiplicacion y multiplicacion
+    elif oper == '*' and oper2 == '*' and oper3 == '+' and oper4 == '=':
+        print(date1 * date2 * date3 + date4)
+    elif oper == '*' and oper2 == '*' and oper3 == '-' and oper4 == '=':
+        print(date1 * date2 * date3 - date4)
+    elif oper == '*' and oper2 == '*' and oper3 == '*' and oper4 == '=':
+        print(date1 * date2 * date3 * date4)
+    elif oper == '*' and oper2 == '*' and oper3 == '/' and oper4 == '=':
+        print(date1 * date2 * date3 / date4)
+    #Multiplicacion y division
+    elif oper == '*' and oper2 == '/' and oper3 == '+' and oper4 == '=':
+        print(date1 * date2 / date3 + date4)
+    elif oper == '*' and oper2 == '/' and oper3 == '-' and oper4 == '=':
+        print(date1 * date2 / date3 - date4)
+    elif oper == '*' and oper2 == '/' and oper3 == '*' and oper4 == '=':
+        print(date1 * date2 / date3 * date4)
+    elif oper == '*' and oper2 == '/' and oper3 == '/' and oper4 == '=':
+        print(date1 * date2 / date3 / date4)
+    #Division
+    #Division y suma
+    elif oper == '/' and oper2 == '+' and oper3 == '+' and oper4 == '=':
+        print(date1 / date2 + date3 + date4)
+    elif oper == '/' and oper2 == '+' and oper3 == '-' and oper4 == '=':
+        print(date1 / date2 + date3 - date4)
+    elif oper == '/' and oper2 == '+' and oper3 == '*' and oper4 == '=':
+        print(date1 / date2 + date3 * date4)
+    elif oper == '/' and oper2 == '+' and oper3 == '/' and oper4 == '=':
+        print(date1 / date2 + date3 / date4)
+    #Division y resta
+    elif oper == '/' and oper2 == '-' and oper3 == '+' and oper4 == '=':
+        print(date1 / date2 - date3 + date4)
+    elif oper == '/' and oper2 == '-' and oper3 == '-' and oper4 == '=':
+        print(date1 / date2 - date3 - date4)
+    elif oper == '/' and oper2 == '-' and oper3 == '*' and oper4 == '=':
+        print(date1 / date2 - date3 * date4)
+    elif oper == '/' and oper2 == '-' and oper3 == '/' and oper4 == '=':
+        print(date1 / date2 - date3 / date4)
+    #Division y multiplicacion
+    elif oper == '/' and oper2 == '*' and oper3 == '+' and oper4 == '=':
+        print(date1 / date2 * date3 + date4)
+    elif oper == '/' and oper2 == '*' and oper3 == '-' and oper4 == '=':
+        print(date1 / date2 * date3 - date4)
+    elif oper == '/' and oper2 == '*' and oper3 == '*' and oper4 == '=':
+        print(date1 / date2 * date3 * date4)
+    elif oper == '/' and oper2 == '*' and oper3 == '/' and oper4 == '=':
+        print(date1 / date2 * date3 / date4)
+    #Division y division
+    elif oper == '/' and oper2 == '/' and oper3 == '+' and oper4 == '=':
+        print(date1 / date2 / date3 + date4)
+    elif oper == '/' and oper2 == '/' and oper3 == '-' and oper4 == '=':
+        print(date1 / date2 / date3 - date4)
+    elif oper == '/' and oper2 == '/' and oper3 == '*' and oper4 == '=':
+        print(date1 / date2 / date3 * date4)
+    elif oper == '/' and oper2 == '/' and oper3 == '/' and oper4 == '=':
+        print(date1 / date2 / date3 / date4)
+    break
